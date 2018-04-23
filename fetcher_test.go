abe276e09ca6ed5ebb5a2a76ced1d5be7345a42e
Yota Toyama <raviqqe@gmail.com>
2018-04-23 14:47:32 +0900

    Test fetcher's cache

--- fetcher_test.go
+++ fetcher_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"net/http"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -10,8 +12,46 @@ func TestNewFetcher(t *testing.T) {
 	newFetcher(1)
 }
 
-func TestFetcherFetchError(t *testing.T) {
-	_, err := newFetcher(1).Fetch("https://google.com/non/existent/path")
+type handler struct{}
 
+func (handler) ServeHTTP(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte("hello"))
+}
+
+func TestFetcherCache(t *testing.T) {
+	go http.ListenAndServe(":8080", handler{})
+	time.Sleep(time.Millisecond)
+
+	f := newFetcher(1)
+
+	p, err := f.Fetch("http://localhost:8080")
+
+	assert.NotEqual(t, (*page)(nil), p)
+	assert.Nil(t, err)
+
+	p, err = f.Fetch("https://foo.com/non/existent/path")
+
+	assert.Equal(t, (*page)(nil), p)
 	assert.NotNil(t, err)
+
+	p, err = f.Fetch("http://localhost:8080")
+
+	assert.Equal(t, (*page)(nil), p)
+	assert.Nil(t, err)
+
+	p, err = f.Fetch("https://foo.com/non/existent/path")
+
+	assert.Equal(t, (*page)(nil), p)
+	assert.NotNil(t, err)
+}
+
+func TestFetcherFetchError(t *testing.T) {
+	f := newFetcher(1)
+
+	for _, s := range []string{"https://foo.com/non/existent/path", ":"} {
+		p, err := f.Fetch(s)
+
+		assert.Equal(t, (*page)(nil), p)
+		assert.NotNil(t, err)
+	}
 }
