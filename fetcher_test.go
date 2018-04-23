a32780a38f91cb96e415aeb63bc77725046b24d5
Yota Toyama <raviqqe@gmail.com>
2018-04-23 15:08:25 +0900

    Run tests locally

--- fetcher_test.go
+++ fetcher_test.go
@@ -8,14 +8,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewFetcher(t *testing.T) {
-	newFetcher(1)
-}
+const existentURL = "http://localhost:8080"
+const nonExistentURL = "http://localhost:8080/hello"
 
 type handler struct{}
 
-func (handler) ServeHTTP(w http.ResponseWriter, _ *http.Request) {
-	w.Write([]byte("hello"))
+func (handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "", "/":
+		w.Write([]byte("hello"))
+	default:
+		w.WriteHeader(404)
+	}
+}
+
+func TestNewFetcher(t *testing.T) {
+	newFetcher(1)
 }
 
 func TestFetcherCache(t *testing.T) {
@@ -24,31 +32,34 @@ func TestFetcherCache(t *testing.T) {
 
 	f := newFetcher(1)
 
-	p, err := f.Fetch("http://localhost:8080")
+	p, err := f.Fetch(existentURL)
 
 	assert.NotEqual(t, (*page)(nil), p)
 	assert.Nil(t, err)
 
-	p, err = f.Fetch("https://foo.com/non/existent/path")
+	p, err = f.Fetch(nonExistentURL)
 
 	assert.Equal(t, (*page)(nil), p)
 	assert.NotNil(t, err)
 
-	p, err = f.Fetch("http://localhost:8080")
+	p, err = f.Fetch(existentURL)
 
 	assert.Equal(t, (*page)(nil), p)
 	assert.Nil(t, err)
 
-	p, err = f.Fetch("https://foo.com/non/existent/path")
+	p, err = f.Fetch(nonExistentURL)
 
 	assert.Equal(t, (*page)(nil), p)
 	assert.NotNil(t, err)
 }
 
 func TestFetcherFetchError(t *testing.T) {
+	go http.ListenAndServe(":8080", handler{})
+	time.Sleep(time.Millisecond)
+
 	f := newFetcher(1)
 
-	for _, s := range []string{"https://foo.com/non/existent/path", ":"} {
+	for _, s := range []string{nonExistentURL, ":"} {
 		p, err := f.Fetch(s)
 
 		assert.Equal(t, (*page)(nil), p)
