389eec1518e19fe50b6b77e1bbbb23372d3a0f90
Yota Toyama <raviqqe@gmail.com>
2018-05-03 10:03:48 +0900

    Add test task

--- rakefile.rb
+++ rakefile.rb
@@ -17,28 +17,30 @@ task :unit_test do
 end
 
 task integration_test: :build do
-  sh './muffet http://localhost:8080'
-  sh '! ./muffet http://localhost:8888'
+  sh './muffet http://localhost:1111'
+  sh '! ./muffet http://localhost:2222'
 
-  sh './muffet -c 1 http://localhost:8080'
-  sh './muffet --concurrency 1 http://localhost:8080'
+  sh './muffet -c 1 http://localhost:1111'
+  sh './muffet --concurrency 1 http://localhost:1111'
 
   sh './muffet --help'
 
-  sh './muffet -v http://localhost:8080 | grep 200'
-  sh '[ $(./muffet -v http://localhost:8080 | wc -l) -eq 14 ]'
-  sh './muffet --verbose http://localhost:8080 | grep 200'
-  sh '! ./muffet http://localhost:8080 | grep 200'
+  sh './muffet -v http://localhost:1111 | grep 200'
+  sh '[ $(./muffet -v http://localhost:1111 | wc -l) -eq 14 ]'
+  sh './muffet --verbose http://localhost:1111 | grep 200'
+  sh '! ./muffet http://localhost:1111 | grep 200'
 
-  sh './muffet -v http://localhost:8080 | sort > /tmp/muffet_1.txt'
-  sh './muffet -v http://localhost:8080 | sort > /tmp/muffet_2.txt'
+  sh './muffet -v http://localhost:1111 | sort > /tmp/muffet_1.txt'
+  sh './muffet -v http://localhost:1111 | sort > /tmp/muffet_2.txt'
   sh 'diff /tmp/muffet_1.txt /tmp/muffet_2.txt'
 
-  sh '! ./muffet http://localhost:8080 | grep .'
+  sh '! ./muffet http://localhost:1111 | grep .'
 end
 
+task test: %w[unit_test integration_test]
+
 task :serve do
-  [['test/valid', 8080], ['test/dead_link', 8888]].each do |args|
+  [['test/valid', 1111], ['test/dead_link', 2222]].each do |args|
     sh "ruby -run -e httpd #{args[0]} -p #{args[1]} &"
   end
 end
