609a04b1d22d988722388b70481882e99b9fb787
Yota Toyama <raviqqe@gmail.com>
2019-10-17 22:06:28 -0700

    Bump patch version

--- arguments.go
+++ arguments.go
@@ -113,7 +113,7 @@ func getArguments(ss []string) (arguments, error) {
 }
 
 func parseArguments(u string, ss []string) map[string]interface{} {
-	args, err := docopt.ParseArgs(u, ss, "1.3.0")
+	args, err := docopt.ParseArgs(u, ss, "1.3.1")
 
 	if err != nil {
 		panic(err)
