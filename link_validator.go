8305024aef96cc8af2f84c65ba9ad3d70d5abd19
Yota Toyama <raviqqe@gmail.com>
2020-09-21 05:49:27 +0000

    Add comment

--- link_validator.go
+++ link_validator.go
@@ -16,6 +16,7 @@ func newLinkValidator(hostname string, robotsData *robotstxt.RobotsData, sitemap
 	return linkValidator{hostname, sitemap, robotsData}
 }
 
+// Validate validates a link and returns true if it is valid as one of an HTML page.
 func (v linkValidator) Validate(u *url.URL) bool {
 	if v.sitemapURLs != nil {
 		if _, ok := v.sitemapURLs[u.String()]; !ok {
