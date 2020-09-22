9b4a99df509856425348ad835b5de530ee3db6ab
Yota Toyama <raviqqe@gmail.com>
2020-09-22 15:57:08 +0000

    Fix tag command

--- tools/docker.sh
+++ tools/docker.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for tag in latest $(git describe --tags); do
+for tag in latest $(git tag --points-at); do
   docker build -t raviqqe/muffet:$tag .
   docker push raviqqe/muffet:$tag
 done
