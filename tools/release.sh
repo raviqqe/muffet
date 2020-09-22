febb85c1f96831802b5b0ed54ca15bfda611b0c1
Yota Toyama <raviqqe@gmail.com>
2020-09-22 05:58:56 +0000

    Add GitHub workflows

---
+++ tools/release.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+if [ -z $CI ]; then
+  exit 1
+fi
+
+version=$(go run . --version)
+
+if git tag -l | grep $version; then
+  exit
+fi
+
+git config --global user.name "$GIT_USER"
+git config --global user.email "$GIT_EMAIL"
+
+git tag $version
+git push --tags
+
+curl -sL https://git.io/goreleaser | bash
