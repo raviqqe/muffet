c7bd5487c87d63df7efb62497e5e1d310d993e53
Yota Toyama <raviqqe@gmail.com>
2020-09-22 06:20:39 +0000

    TEST release

--- tools/release.sh
+++ tools/release.sh
@@ -8,14 +8,14 @@ fi
 
 version=$(go run . --version)
 
-if git tag -l | grep $version; then
-  exit
-fi
+# if git tag -l | grep $version; then
+#   exit
+# fi
 
 git config --global user.name "$GIT_USER"
 git config --global user.email "$GIT_EMAIL"
 
 git tag $version
-git push --tags
+# git push --tags
 
-curl -sL https://git.io/goreleaser | bash
+# curl -sL https://git.io/goreleaser | bash
