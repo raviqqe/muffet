dc8e8c13669d33f6d5e5df58a48af8b84884011e
Yota Toyama <raviqqe@gmail.com>
2020-09-23 05:00:31 +0000

    Fix docker version tag

--- tools/docker.sh
+++ tools/docker.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for tag in latest $(git tag --points-at); do
+for tag in latest $(git tag --points-at | sed s/^v//); do
   docker build -t raviqqe/muffet:$tag .
   docker push raviqqe/muffet:$tag
 done
