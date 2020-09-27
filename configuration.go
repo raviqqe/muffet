47881a09e41a28d0372450b8627c74f26b105d7e
Yota Toyama <raviqqe@gmail.com>
2020-09-27 03:31:21 +0000

    Remove unused constant

--- configuration.go
+++ configuration.go
@@ -3,9 +3,8 @@ package main
 import "time"
 
 const (
-	version               = "2.0.2"
-	agentName             = "muffet"
-	concurrency           = 1024
-	tcpTimeout            = time.Minute
-	defaultMaxConnections = 512
+	version     = "2.0.2"
+	agentName   = "muffet"
+	concurrency = 1024
+	tcpTimeout  = time.Minute
 )
