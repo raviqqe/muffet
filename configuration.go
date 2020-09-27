68f61361180948335cdbf87ccca3bf1b65484f62
Yota Toyama <raviqqe@gmail.com>
2020-09-27 03:26:46 +0000

    Bump version

--- configuration.go
+++ configuration.go
@@ -3,7 +3,7 @@ package main
 import "time"
 
 const (
-	version               = "2.0.1"
+	version               = "2.0.2"
 	agentName             = "muffet"
 	concurrency           = 1024
 	tcpTimeout            = time.Minute
