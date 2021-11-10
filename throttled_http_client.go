384b08d5a89eca36a3d69c99dffe2a9573034484
Yota Toyama <raviqqe@gmail.com>
2021-11-10 05:26:52 +0000

    Fix host limiting

--- throttled_http_client.go
+++ throttled_http_client.go
@@ -8,26 +8,57 @@ import (
 
 // TODO Throttle requests for each host.
 type throttledHttpClient struct {
-	client    httpClient
-	limiter   ratelimit.Limiter
-	semaphore semaphore
+	client                httpClient
+	connections           semaphore
+	hosts                 map[string]*Host
+	requestPerSecond      int
+	maxConnectionsPerHost int
 }
 
-func newThrottledHttpClient(c httpClient, rps int, maxConnections int) httpClient {
-	l := ratelimit.NewUnlimited()
+type Host struct {
+	limiter     ratelimit.Limiter
+	connections semaphore
+}
 
-	if rps > 0 {
-		l = ratelimit.New(rps)
+func newThrottledHttpClient(c httpClient, requestPerSecond int, maxConnections, maxConnectionsPerHost int) httpClient {
+	return &throttledHttpClient{
+		c,
+		newSemaphore(maxConnections),
+		map[string]*Host{},
+		requestPerSecond,
+		maxConnectionsPerHost,
 	}
-
-	return &throttledHttpClient{c, l, newSemaphore(maxConnections)}
 }
 
 func (c *throttledHttpClient) Get(u *url.URL) (httpResponse, error) {
-	c.semaphore.Request()
-	defer c.semaphore.Release()
+	c.connections.Request()
+	defer c.connections.Release()
 
-	c.limiter.Take()
+	h := c.getHost(u.Hostname())
+	h.limiter.Take()
+	h.connections.Request()
+	defer h.connections.Release()
 
 	return c.client.Get(u)
+}
+
+func (c *throttledHttpClient) getHost(name string) *Host {
+	h, ok := c.hosts[name]
+
+	if !ok {
+		h = &Host{c.createLimiter(), newSemaphore(c.maxConnectionsPerHost)}
+		c.hosts[name] = h
+	}
+
+	return h
+}
+
+func (c *throttledHttpClient) createLimiter() ratelimit.Limiter {
+	l := ratelimit.NewUnlimited()
+
+	if c.requestPerSecond > 0 {
+		l = ratelimit.New(c.requestPerSecond)
+	}
+
+	return l
 }
