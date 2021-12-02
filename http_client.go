1edb5c521e9864479938d44ee396a0272021ec61
Yota Toyama <raviqqe@gmail.com>
2021-12-02 07:03:08 +0000

    Fix typos

--- http_client.go
+++ http_client.go
@@ -5,7 +5,7 @@ import (
 )
 
 type httpClient interface {
-	// Get sends a HTTP request with the GET method.
+	// Get sends an HTTP request with a GET method.
 	// Non-200 status codes always result in non-nil errors.
 	Get(url *url.URL) (httpResponse, error)
 }
