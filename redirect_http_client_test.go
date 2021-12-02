f2a21acc79f88882fd22011b11eaffed4bed59df
Yota Toyama <raviqqe@gmail.com>
2021-12-02 07:58:26 +0000

    Test redirect HTTP client more

--- redirect_http_client_test.go
+++ redirect_http_client_test.go
@@ -40,3 +40,70 @@ func TestRedirectHttpClientGet(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, 200, r.StatusCode())
 }
+
+func TestRedirectHttpClientGetWithRedirect(t *testing.T) {
+	u, err := url.Parse(testUrl)
+
+	assert.Nil(t, err)
+
+	redirected := false
+	r, err := newRedirectHttpClient(
+		newFakeHttpClient(
+			func(u *url.URL) (*fakeHttpResponse, error) {
+				if u.String() != testUrl {
+					return nil, errors.New("")
+				} else if !redirected {
+					redirected = true
+
+					return newFakeHttpResponse(
+						300,
+						"http://foo.com",
+						nil,
+						map[string]string{"Location": "http://foo.com"},
+					), nil
+				}
+
+				return newFakeHtmlResponse("http://foo.com", ""), nil
+			},
+		),
+		42,
+	).Get(u)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 200, r.StatusCode())
+}
+
+func TestRedirectHttpClientGetWithRedirects(t *testing.T) {
+	const maxRedirections = 42
+
+	u, err := url.Parse(testUrl)
+
+	assert.Nil(t, err)
+
+	i := 0
+	r, err := newRedirectHttpClient(
+		newFakeHttpClient(
+			func(u *url.URL) (*fakeHttpResponse, error) {
+				if u.String() != testUrl {
+					return nil, errors.New("")
+				} else if i < maxRedirections {
+					i++
+
+					return newFakeHttpResponse(
+						300,
+						"http://foo.com",
+						nil,
+						map[string]string{"Location": "http://foo.com"},
+					), nil
+				}
+
+				return newFakeHtmlResponse("http://foo.com", ""), nil
+			},
+		),
+		maxRedirections,
+	).Get(u)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 200, r.StatusCode())
+	assert.Equal(t, maxRedirections, i)
+}
