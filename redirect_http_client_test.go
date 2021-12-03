3e88bbf6104b36069bb039c310f0f646eb355e3a
Yota Toyama <raviqqe@gmail.com>
2021-12-03 03:06:01 +0000

    Add missing unit tests

--- redirect_http_client_test.go
+++ redirect_http_client_test.go
@@ -145,3 +145,91 @@ func TestRedirectHttpClientGetWithRelativeRedirect(t *testing.T) {
 	assert.Equal(t, 200, r.StatusCode())
 	assert.Equal(t, redirected, true)
 }
+
+func TestRedirectHttpClientFailWithTooManyRedirects(t *testing.T) {
+	const maxRedirections = 42
+
+	u, err := url.Parse(testUrl)
+
+	assert.Nil(t, err)
+
+	i := 0
+	r, err := newRedirectHttpClient(
+		newFakeHttpClient(
+			func(u *url.URL) (*fakeHttpResponse, error) {
+				i++
+
+				return newFakeHttpResponse(
+					300,
+					"http://foo.com",
+					nil,
+					map[string]string{"location": "http://foo.com"},
+				), nil
+			},
+		),
+		maxRedirections,
+	).Get(u)
+
+	assert.Nil(t, r)
+	assert.Equal(t, err.Error(), "too many redirections")
+	assert.Equal(t, maxRedirections+1, i)
+}
+
+func TestRedirectHttpClientFailWithUnsetLocationHeader(t *testing.T) {
+	u, err := url.Parse(testUrl)
+
+	assert.Nil(t, err)
+
+	r, err := newRedirectHttpClient(
+		newFakeHttpClient(
+			func(u *url.URL) (*fakeHttpResponse, error) {
+				return newFakeHttpResponse(300, "http://foo.com", nil, nil), nil
+			},
+		),
+		42,
+	).Get(u)
+
+	assert.Nil(t, r)
+	assert.Equal(t, err.Error(), "location header not set")
+}
+
+func TestRedirectHttpClientFailWithInvalidLocationURL(t *testing.T) {
+	u, err := url.Parse(testUrl)
+
+	assert.Nil(t, err)
+
+	r, err := newRedirectHttpClient(
+		newFakeHttpClient(
+			func(u *url.URL) (*fakeHttpResponse, error) {
+				return newFakeHttpResponse(
+					300,
+					"http://foo.com",
+					nil,
+					map[string]string{"location": ":"},
+				), nil
+			},
+		),
+		42,
+	).Get(u)
+
+	assert.Nil(t, r)
+	assert.Contains(t, err.Error(), "parse")
+}
+
+func TestRedirectHttpClientFailWithInvalidStatusCode(t *testing.T) {
+	u, err := url.Parse(testUrl)
+
+	assert.Nil(t, err)
+
+	r, err := newRedirectHttpClient(
+		newFakeHttpClient(
+			func(u *url.URL) (*fakeHttpResponse, error) {
+				return newFakeHttpResponse(404, "http://foo.com", nil, nil), nil
+			},
+		),
+		42,
+	).Get(u)
+
+	assert.Nil(t, r)
+	assert.Equal(t, err.Error(), "404")
+}
