21c8d621595cf25214beee1bdd1a69e1046a9bf6
Yota Toyama <raviqqe@gmail.com>
2023-01-29 17:56:42 +0800

    Refactor

--- page_result_test.go
+++ page_result_test.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestPageResultOK(t *testing.T) {
-	d, _ := time.ParseDuration("1s")
-	assert.True(t, (&pageResult{"", nil, nil, d}).OK())
-	assert.False(t, (&pageResult{"", nil, []*errorLinkResult{{}}, d}).OK())
+	assert.True(t, (&pageResult{"", nil, nil, 0}).OK())
+	assert.False(t, (&pageResult{"", nil, []*errorLinkResult{{}}, 0}).OK())
 }
