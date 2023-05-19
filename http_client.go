1310dbcfae32a0dec43548f078ed81c4d0719cca
Yota Toyama <raviqqe@gmail.com>
2023-05-19 13:26:25 +1000

    Fix

--- http_client.go
+++ http_client.go
@@ -8,5 +8,5 @@ import (
 type httpClient interface {
 	// Get sends an HTTP request with a GET method.
 	// It depends on implementation of each client what is considered as errors.
-	Get(url *url.URL, headers http.Header) (httpResponse, error)
+	Get(url *url.URL, header http.Header) (httpResponse, error)
 }
