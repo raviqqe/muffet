74671e4ca0fe265cd3077f18be0f4d043f49110b
Yota Toyama <raviqqe@gmail.com>
2023-06-28 16:09:22 +1000

    Fix

--- page.go
+++ page.go
@@ -9,25 +9,3 @@ type page interface {
 	Fragments() map[string]struct{}
 	Links() map[string]error
 }
-
-type htmlPage struct {
-	url       *url.URL
-	fragments map[string]struct{}
-	links     map[string]error
-}
-
-func newHtmlPage(u *url.URL, fragments map[string]struct{}, links map[string]error) *htmlPage {
-	return &htmlPage{u, fragments, links}
-}
-
-func (p *htmlPage) URL() *url.URL {
-	return p.url
-}
-
-func (p *htmlPage) Fragments() map[string]struct{} {
-	return p.fragments
-}
-
-func (p *htmlPage) Links() map[string]error {
-	return p.links
-}
