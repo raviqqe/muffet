ea8b7c7fb0256b8478552bf92041f7d57ea04eae
Yota Toyama <raviqqe@gmail.com>
2023-06-29 12:33:25 +1000

    Fix

--- link_filterer.go
+++ link_filterer.go
@@ -27,11 +27,11 @@ func newLinkFilterer(es []*regexp.Regexp, is []*regexp.Regexp) linkFilterer {
 func (f linkFilterer) Filter(u *url.URL) bool {
 	s := u.String()
 
-	if _, ok := validSchemes[u.Scheme]; ok && !f.isLinkExcluded(s) && f.isLinkIncluded(s) {
-		return true
+	if _, ok := validSchemes[u.Scheme]; !ok {
+		return false
 	}
 
-	return false
+	return !f.isLinkExcluded(s) && f.isLinkIncluded(s)
 }
 
 func (linkFilterer) parseLinks(n *html.Node, a string) []string {
