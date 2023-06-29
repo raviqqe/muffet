c4ff213e6d48282266071518c73c269fb1c4dc85
Yota Toyama <raviqqe@gmail.com>
2023-06-29 12:29:35 +1000

    Fix

---
+++ link_filterer.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"net/url"
+	"regexp"
+	"strings"
+
+	"github.com/yhat/scrape"
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
+)
+
+var validSchemes = map[string]struct{}{
+	"":      {},
+	"http":  {},
+	"https": {},
+}
+
+var atomToAttributes = map[atom.Atom][]string{
+	atom.A:      {"href"},
+	atom.Frame:  {"src"},
+	atom.Iframe: {"src"},
+	atom.Img:    {"src"},
+	atom.Link:   {"href"},
+	atom.Script: {"src"},
+	atom.Source: {"src", "srcset"},
+	atom.Track:  {"src"},
+	atom.Meta:   {"content"},
+}
+
+var imageDescriptorPattern = regexp.MustCompile(" [^ ]*$")
+
+type linkFilterer struct {
+	excludedPatterns []*regexp.Regexp
+	includedPatterns []*regexp.Regexp
+}
+
+func newLinkFilterer(es []*regexp.Regexp, is []*regexp.Regexp) linkFilterer {
+	return linkFilterer{excludedPatterns: es, includedPatterns: is}
+}
+
+func (f linkFilterer) Find(n *html.Node, base *url.URL) map[string]error {
+	ls := map[string]error{}
+
+	for _, n := range scrape.FindAllNested(n, func(n *html.Node) bool {
+		_, ok := atomToAttributes[n.DataAtom]
+		return ok
+	}) {
+		for _, a := range atomToAttributes[n.DataAtom] {
+			ss := f.parseLinks(n, a)
+
+			for _, s := range ss {
+				s := strings.TrimSpace(s)
+
+				if s == "" {
+					continue
+				}
+
+				u, err := url.Parse(s)
+				if err != nil {
+					ls[s] = err
+					continue
+				}
+
+				s = base.ResolveReference(u).String()
+
+				if _, ok := validSchemes[u.Scheme]; ok && !f.isLinkExcluded(s) && f.isLinkIncluded(s) {
+					ls[s] = nil
+				}
+			}
+		}
+	}
+
+	return ls
+}
+
+func (linkFilterer) parseLinks(n *html.Node, a string) []string {
+	s := scrape.Attr(n, a)
+	ss := []string{}
+
+	switch a {
+	case "srcset":
+		for _, s := range strings.Split(s, ",") {
+			ss = append(ss, imageDescriptorPattern.ReplaceAllString(strings.TrimSpace(s), ""))
+		}
+	case "content":
+		switch scrape.Attr(n, "property") {
+		case "og:image", "og:audio", "og:video", "og:image:url", "og:image:secure_url", "twitter:image":
+			ss = append(ss, s)
+		}
+	default:
+		ss = append(ss, s)
+	}
+
+	return ss
+}
+
+func (f linkFilterer) isLinkExcluded(u string) bool {
+	return f.matches(u, f.excludedPatterns)
+}
+
+func (f linkFilterer) isLinkIncluded(u string) bool {
+	return len(f.includedPatterns) == 0 || f.matches(u, f.includedPatterns)
+}
+
+func (f linkFilterer) matches(u string, rs []*regexp.Regexp) bool {
+	for _, r := range rs {
+		if r.MatchString(u) {
+			return true
+		}
+	}
+
+	return false
+}
