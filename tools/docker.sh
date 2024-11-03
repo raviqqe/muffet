10e288c7593d7a0319d4e281fd6762fca99a7927
Yota Toyama <raviqqe@gmail.com>
2024-11-03 17:02:59 +0900

    Build Docker image for RISC-V

--- tools/docker.sh
+++ tools/docker.sh
@@ -4,7 +4,7 @@ set -e
 
 for tag in latest $(git tag --points-at | sed s/^v//); do
   docker buildx build \
-    --platform linux/amd64,linux/arm64 \
+    --platform linux/amd64,linux/arm64,linux/riscv64 \
     --tag raviqqe/muffet:$tag \
     --push \
     .
