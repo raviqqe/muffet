51f271eee80ea0facd9bd0caf5fcffbe15cdfa8b
Yota Toyama <raviqqe@gmail.com>
2024-11-03 17:07:46 +0900

    Try other platforms in Docker build

--- tools/docker.sh
+++ tools/docker.sh
@@ -4,7 +4,7 @@ set -e
 
 for tag in latest $(git tag --points-at | sed s/^v//); do
   docker buildx build \
-    --platform linux/amd64,linux/arm64,linux/riscv64 \
+    --platform linux/386,linux/amd64,linux/arm,linux/arm64,windows/amd64 \
     --tag raviqqe/muffet:$tag \
     --push \
     .
