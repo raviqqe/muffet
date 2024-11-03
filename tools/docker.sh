2f60582af7af80f658aa891e33d68a349bc740df
Yota Toyama <raviqqe@gmail.com>
2024-11-03 17:14:02 +0900

    Disable Windows build (#421)

--- tools/docker.sh
+++ tools/docker.sh
@@ -4,7 +4,7 @@ set -e
 
 for tag in latest $(git tag --points-at | sed s/^v//); do
   docker buildx build \
-    --platform linux/386,linux/amd64,linux/arm,linux/arm64,windows/amd64 \
+    --platform linux/386,linux/amd64,linux/arm,linux/arm64 \
     --tag raviqqe/muffet:$tag \
     --push \
     .
