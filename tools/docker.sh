726091e2b69adb67a70773542b41263821a82e90
Yota Toyama <raviqqe@gmail.com>
2024-11-03 16:49:09 +0900

    Refactor Docker (#418)

--- tools/docker.sh
+++ tools/docker.sh
@@ -2,30 +2,10 @@
 
 set -e
 
-check_buildx(){
-    docker buildx version &>/dev/null
-    return $?
-}
-
-build_image() {
-    local platforms=${1:-} 
-    if [ ! -z "$platforms" ] && check_buildx; then
-        echo "Building for platforms: $platforms"
-        # Build for each git tag and latest
-        for tag in latest $(git tag --points-at | sed s/^v//); do
-          docker buildx build \
-             --platform "$platforms" \
-             --tag raviqqe/muffet:$tag \
-             --push \
-             .
-        done
-    else
-        for tag in latest $(git tag --points-at | sed s/^v//); do
-          docker build -t raviqqe/muffet:$tag .
-          docker push raviqqe/muffet:$tag
-        done
-    fi
-}
-
-
-build_image "$@"
+for tag in latest $(git tag --points-at | sed s/^v//); do
+  docker buildx build \
+    --platform linux/amd64,linux/arm64 \
+    --tag raviqqe/muffet:$tag \
+    --push \
+    .
+done
