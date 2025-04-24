31ceffef0a609cae84e8ce95d1b475feae9758ec
Yota Toyama <raviqqe@gmail.com>
2025-04-23 19:55:48 -0700

    Fix

--- doc/astro.config.ts
+++ doc/astro.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
   integrations: [
     sitemap(),
     starlight({
-      title: "Muffet Scheme",
+      title: "Muffet",
       customCss: ["./src/index.css"],
       favicon: "/icon.svg",
       head: [
