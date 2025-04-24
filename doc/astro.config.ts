ae9728dea999c280f08c87e75d2ad713405b2afb
Yota Toyama <raviqqe@gmail.com>
2025-04-23 20:09:22 -0700

    Fix

--- doc/astro.config.ts
+++ doc/astro.config.ts
@@ -1,42 +1,6 @@
-import { readFile, readdir, stat } from "node:fs/promises";
-import { join, parse } from "node:path";
 import sitemap from "@astrojs/sitemap";
 import starlight from "@astrojs/starlight";
 import { defineConfig } from "astro/config";
-import { capitalize, sortBy } from "es-toolkit";
-
-type Item = { label: string; link: string } | { label: string; items: Item[] };
-
-const documentDirectory = "src/content/docs";
-
-const listItems = async (directory: string): Promise<Item[]> =>
-  sortBy(
-    await Promise.all(
-      (await readdir(join(documentDirectory, directory)))
-        .filter((path) => !path.startsWith("."))
-        .map(async (path) => {
-          const fullPath = join(documentDirectory, directory, path);
-          const { name } = parse(path);
-          const linkPath = join(directory, name);
-
-          return (await stat(fullPath)).isDirectory()
-            ? {
-                label: capitalize(name.replace("-", " ")),
-                items: await listItems(linkPath),
-              }
-            : {
-                label:
-                  (await readFile(fullPath, "utf-8"))
-                    .split("\n")
-                    .find((line) => line.startsWith("title: "))
-                    ?.replace("title: ", "")
-                    .trim() ?? "",
-                link: linkPath,
-              };
-        }),
-    ),
-    [({ label, link }) => [!link, label]],
-  );
 
 export default defineConfig({
   base: "/muffet",
