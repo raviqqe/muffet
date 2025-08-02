a35c400be6ec12e8bd7a7fce3f2f33d7614ae90b
Yota Toyama <raviqqe@gmail.com>
2025-08-02 10:44:38 -0700

    Refactor

--- retry_http_client_test.go
+++ retry_http_client_test.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"net/url"
+	"sync/atomic"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 type fakeNetError struct{}
@@ -10,61 +15,43 @@ func (fakeNetError) Error() string   { return "network error" }
 func (fakeNetError) Timeout() bool   { return true }
 func (fakeNetError) Temporary() bool { return true }
 
-func TestPageCheckerCheckPageRetry(t *testing.T) {
+func TestRetryHttpClientGet(t *testing.T) {
+	u, err := url.Parse("http://foo.com/foo")
+	assert.Nil(t, err)
+
 	for _, tt := range []struct {
-		name             string
-		errCnt           int
+		errorCount       int
 		expectedRequests int
 		success          bool
 	}{
-		{name: "no errors", errCnt: 0, expectedRequests: 1, success: true},
-		{name: "2 errors", errCnt: 2, expectedRequests: 3, success: true},
-		{name: "3 errors", errCnt: 3, expectedRequests: 3, success: false},
+		{errorCount: 0, expectedRequests: 1, success: true},
+		{errorCount: 1, expectedRequests: 2, success: true},
+		{errorCount: 2, expectedRequests: 3, success: true},
+		{errorCount: 3, expectedRequests: 3, success: false},
 	} {
 		t.Run(
-			tt.name,
+			fmt.Sprintf("%d retries", tt.errorCount),
 			func(t *testing.T) {
-				var reqCnt atomic.Int32
-				c := newTestPageCheckerWithRetries(
+				var count atomic.Int32
+				c := newRetryHttpClient(
 					newFakeHttpClient(
 						func(u *url.URL) (*fakeHttpResponse, error) {
 							if u.String() == "http://foo.com/foo" {
-								if reqCnt.Add(1) <= int32(tt.errCnt) {
+								if count.Add(1) <= int32(tt.errorCount) {
 									return nil, &fakeNetError{}
 								}
 								return newFakeHtmlResponse("http://foo.com/foo", ""), nil
 							}
 							return newFakeHtmlResponse("http://foo.com/", ""), nil
 						},
-					), 3,
+					),
+					3,
 				)
 
-				go c.Check(
-					newTestPage(t, nil, map[string]error{"http://foo.com/foo": nil}),
-				)
-
-				i := 0
-
-				for r := range c.Results() {
-					i++
-					if tt.success {
-						assert.True(t, r.OK())
-					} else {
-						assert.False(t, r.OK())
-						assert.Len(t, r.ErrorLinkResults, 1)
-						assert.Len(t, r.SuccessLinkResults, 0)
-						assert.Equal(t, "http://foo.com/foo", r.ErrorLinkResults[0].URL)
-					}
-				}
-
-				if tt.success {
-					// initial page + 1 crawled page
-					assert.Equal(t, 2, i)
-				} else {
-					// the crawled page failed
-					assert.Equal(t, 1, i)
-				}
-				assert.Equal(t, int32(tt.expectedRequests), reqCnt.Load())
+				r, err := c.Get(u, nil)
+				assert.Equal(t, tt.success, r != nil)
+				assert.Equal(t, tt.success, err == nil)
+				assert.Equal(t, int32(tt.expectedRequests), count.Load())
 			},
 		)
 	}
