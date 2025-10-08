8b9aac8af199967945f2c822696d959ba4826714
alonso113 <geesinkantonalonso@gmail.com>
2025-10-08 12:27:50 +0200

    temp changes to main.go for testing purposes

--- main.go
+++ main.go
@@ -9,11 +9,24 @@ import (
 )
 
 func main() {
+	//change to nicer implementation later, for testing
+	useTls := false
+	for _, a := range os.Args[1:] {
+		if a == "--use-tls-client" {
+			useTls = true
+		}
+	}
+	var factory httpClientFactory
+	factory = newFasthttpHttpClientFactory()
+	if useTls {
+		factory = newTlsHttpClientFactory()
+	}
+
 	ok := newCommand(
 		colorable.NewColorableStdout(),
 		os.Stderr,
 		isatty.IsTerminal(os.Stdout.Fd()),
-		newFasthttpHttpClientFactory(),
+		factory,
 	).Run(os.Args[1:])
 
 	if !ok {
